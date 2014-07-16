9ea9c5e7ac1a82023699fe00cedf75ddf7556fda
Taku Fukata <fukatataku@gmail.com>
2014-07-17 00:22:47 +0900

    add init.el

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # -*- encoding:utf-8 -*-
 
-HERE=$(readlink -f $(dirname $0))
+#HERE=$(readlink -f $(dirname $0))
+HERE=$(cd $(dirname $0);pwd)
 
 mkdir -p ~/.vim
+mkdir -p ~/.emacs.d
 
 ln -sf $HERE/vimrc ~/.vim/vimrc
 ln -sf $HERE/bash_profile ~/.bash_profile
+ln -sf $HERE/init.el ~/.emacs.d/init.el
 
