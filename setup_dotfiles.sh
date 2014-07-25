683ef9b9972fefb2fcad2d3c8fdb64872e81841d
Taku Fukata <fukatataku@gmail.com>
2014-07-25 16:49:38 +0900

    update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,10 +4,14 @@
 #HERE=$(readlink -f $(dirname $0))
 HERE=$(cd $(dirname $0);pwd)
 
-mkdir -p ~/.vim
+mkdir -p ~/.vim/bundle
 mkdir -p ~/.emacs.d
 
 ln -sf $HERE/vimrc ~/.vim/vimrc
 ln -sf $HERE/bash_profile ~/.bash_profile
 ln -sf $HERE/init.el ~/.emacs.d/init.el
 ln -sf $HERE/inputrc ~/.inputrc
+
+if [ ! -e ~/.vim/bundle/neobundle.vimm ]; then
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
